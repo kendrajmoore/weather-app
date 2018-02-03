4a556e65485c4c17b1601028e88e0854e1e862ea
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-03 10:22:56 -0500

    more python

---
+++ python-exercise/index.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+class Deck:
+    def_init_(self, card):
+        self.card = card
+
+    def deal(self):
+        pass
+
+    def shuffle(self):
+        if len(deck < 52)
+
+
+class Card:
+    def_init_(self, suit, value):
+        self.suit = suit
+        self.value = value
