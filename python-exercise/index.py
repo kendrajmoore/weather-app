13594e7d25f5f02b1346b640e5dc68e480a5bb92
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-03 10:32:48 -0500

    python

--- python-exercise/index.py
+++ python-exercise/index.py
@@ -1,14 +1,21 @@
 from random import shuffle
 
 class Deck:
-    def_init_(self, card):
-        self.card = card
+    def_init_(self):
+        suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
+        values = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'K', ]
+        self.cards = [Card(suit, value) for suit in suits for value in values]
 
     def deal(self):
-        pass
+        if len(self.card) == 0:
+            print("sorry no cards")
+        return self.cards.pop()
 
     def shuffle(self):
-        if len(deck < 52)
+        if len(self.cards) < 52:
+            print("You messed up")
+        shuffle(self.card)
+        return self
 
 
 class Card:
