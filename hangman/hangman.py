823f9973acd02bbfaebeeb9270477bb20812c3ef
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-05 21:09:43 -0500

    learning socket.io

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -1,1 +1,41 @@
 import random
+HANGMAN_PICS = ['''
+
+  +---+
+      |
+      |
+      |
+     ===''', '''
+  +---+
+ o    |
+      |
+      |
+     === ''', '''
+  +---+
+  o   |
+  |   |
+      |
+     === ''', '''
+ +---+
+ o   |
+/|   |
+     |
+    === ''', '''
+ +---+
+ o   |
+/|\  |
+     |
+    === ''', '''
+ +---+
+ o   |
+/|\  |
+/    |
+    === ''', '''
+ +---+
+ o   |
+/|\  |
+/ \  |
+    === ''']
+word = 'ant baboon badger bat bear beaver camel cat clam'.split()
+
+print('Missed letters: ')
