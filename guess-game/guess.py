fe82feec25937fd756c30b2d4b6ef404782a8d04
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-10 20:57:08 -0500

    game

--- guess-game/guess.py
+++ guess-game/guess.py
@@ -1,1 +1,12 @@
 import random
+
+random_number = random.randit(1, 11)
+
+answer = input('Guess a number between 1 and 10: ')
+
+if answer > random_number:
+    print('Too high, please try again')
+if answer < random_number:
+    print('Too low, please try again')
+else:
+    print('You win!')
