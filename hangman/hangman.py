58237c1389e8fed106a6732742e9cb1dbb30f709
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-20 13:30:23 -0800

    airport

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -36,11 +36,11 @@ HANGMAN_PICS = ['''
 /|\  |
 / \  |
     === ''']
-word = 'ant baboon badger bat bear beaver camel cat clam'.split()
+words = 'ant baboon badger bat bear beaver camel cat clam'.split()
 
 
-def printWord(wordList):
-    wordIndex = random.randint(0, len(word) - 1)
+def getRandomWord(wordList):
+    wordIndex = random.randint(0, len(wordList) - 1)
     return wordList[wordIndex]
 
 
@@ -62,3 +62,60 @@ def displayBoard(missedLetters, correctLetters, secretWord):
     for letter in blanks:
         print(letter, end=' ')
     print()
+
+
+def getGuess(alreadyGuessed):
+    while True:
+        print('Guess a letter')
+        guess = input()
+        guess = guess.lower
+        if len(guess) != 1:
+            print('Please enter a single letter.')
+        elif guess in alreadyGuessed:
+            print('You have already guessed that')
+        elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter a LETTER')
+        else:
+            return guess
+
+
+def playAgain():
+        print('Do you want to play again')
+        return input().lower().startswith('y')
+
+
+print('H A N G M A N')
+missedLetters = ''
+correctLetters = ''
+secretWord = getRandomWord(words)
+gameIsDone = False
+
+while True:
+    displayBoard(missedLetters, correctLetters, secretWord)
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+        if foundAllLetters:
+                print('Yes')
+                gameIsDone = True
+    else:
+        missedLetters = missedLetters + guess
+        if len(missedLetters) == len(HANGMAN_PICS) - 1:
+            displayBoard(missedLetters, correctLetters, secretWord)
+            print('No more guesses')
+            gameIsDone = True
+
+        if gameIsDone:
+            if playAgain():
+                missedLetters = ''
+                correctLetters = ''
+                gameIsDone = False
+                secretWord = getRandomWord(words)
+            else:
+                break
--- tic-tac-toe/tic.py
+++ tic-tac-toe/tic.py
@@ -1,24 +1,159 @@
+import random
 
 
-print('...rock...')
-print('...paper...')
-print('...scissors...')
+def drawBoard(board):
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print('- + - + -')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('- + - + -')
+    print(board[1] + '|' + board[2] + '|' + board[3])
 
-player1 = input('Player 1, make your move: ')
-player2 = input('Player 2, make your move: ')
 
-if player1 == 'rock' and player2 == 'scissors':
-    print('player1 wins!')
-elif player1 == 'rock' and player2 == 'paper':
-    print('player2 wins!')
-elif player1 == 'paper' and player2 == 'rock':
-    print('player1 wins!')
-elif player1 == 'paper' and player2 == 'scissors':
-    print('player2 wins!')
-elif player1 == 'scissors' and player2 == 'rock':
-    print('player2 wins')
+def inputPlayerLetter():
+    letter = ''
+    while not (letter == 'X' or 'O'):
+        print('Do you want to be X or O')
+        letter = input().upper
+    if letter == 'X':
+        return['X', 'O']
+    else:
+        return['O', 'X']
 
-elif player1 == player2:
-    print("It's a tie")
-else:
-    print('broken')
+
+def whoGoesFirst():
+    if random.randint(0, 1) == 0:
+        return 'computer'
+    else:
+        return 'player'
+
+
+def makeMove(board, letter, move):
+    board[move] = letter
+
+
+def isWinner(bo, le):
+    return ((bo[7] == le and bo[8] == le and bo[9] == le) or
+            (bo[4] == le and bo[5] == le and bo[6] == le) or
+            (bo[1] == le and bo[2] == le and bo[3] == le) or
+            (bo[7] == le and bo[4] == le and bo[1] == le) or
+            (bo[8] == le and bo[5] == le and bo[2] == le) or
+            (bo[9] == le and bo[6] == le and bo[3] == le) or
+            (bo[7] == le and bo[5] == le and bo[3] == le) or
+            (bo[9] == le and bo[5] == le and bo[1] == le))
+
+
+def getBoardCopy(board):
+    boardCopy = []
+    for i in board:
+        boardCopy.append(i)
+    return boardCopy
+
+
+def isSpaceFree(board, move):
+    return board[move] == ' '
+
+
+def getPlayerMove(board):
+    move = ' '
+    while move['1', '2', '3', '4', '5', '6', '7', '8', '9'] or
+    isSpaceFree(board, int(move)):
+            print('What is your next move')
+            move = input()
+    return int(move)
+
+
+def chooseRandomMoveFromList(board, movesList):
+    for i in movesList:
+        if isSpaceFree(board, i):
+            possibleMoves.append(i)
+
+    if len(possibleMoves) != 0:
+        return random.choice(possibleMoves)
+    else:
+        return None
+
+
+def getComputerMove(board, computerLetter):
+    if computerLetter == 'X':
+        playerLetter = '0'
+    else:
+        playerLetter = 'X'
+
+    for i in range(1, 10):
+        boardCopy = getBoardCopy(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, computerLetter, i)
+            if isWinner(boardCopy, computerLetter):
+                return i
+
+    for i in range(1, 10):
+        boardCopy = getBoardCopy(board)
+        if isSpaceFree(boardCopy, i):
+            makeMove(boardCopy, computerLetter, i)
+            if isWinner(boardCopy, computerLetter):
+                return i
+
+    move = chooseRandomMoveFromList(board, [1, 3, 7, 9])
+    if move not None:
+        return move
+
+    if isSpaceFree(board, 5):
+        return 5
+
+    return chooseRandomMoveFromList(board, [2, 4, 6, 8])
+
+
+def isBoardFull(board):
+    for i in range(1, 10):
+        if isSpaceFree(board, i):
+            return False
+        return True
+
+
+print('Welcome to Tic-Tac_Toe')
+
+while True:
+
+    theBoard = [' '] * 10
+    playerLetter, computerLetter = inputPlayerLetter()
+    turn = whoGoesFirst()
+    print('The ' + turn + ' will go first')
+    gameIsPlaying = True
+
+    while gameIsPlaying:
+        if turn == 'player':
+            drawBoard(theBoard)
+            move = getPlayerMove(theBoard)
+            makeMove(theBoard, playerLetter, move)
+
+            if isWinne(theBoard, playerLetter):
+                drawBoard(theBoard)
+                print('Hooray!')
+                gameIsPlaying = False
+            else:
+                if isBoardFull(theBoard):
+                    drawBoard(theBoard)
+                    print('Tie')
+                    break
+                else:
+                    turn = 'computer'
+
+        else:
+            move = getComputerMove(theBoard, computerLetter)
+            makeMove(theBoard, computerLetter, move)
+
+            if isWinner(theBoard, computerLetter):
+                drawBoard(theBoard)
+                print('The computer won!')
+                gameIsPlaying = False
+            else:
+                if isBoardFull(theBoard):
+                    drawBoard(theBoard)
+                    print('Game is a tie')
+                    break
+                else:
+                    turn = 'player'
+
+    print('Do you want to play again?')
+    if not input().lower().startswith('y'):
+        break
