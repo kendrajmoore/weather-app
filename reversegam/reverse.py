0b0320d782723b26f27fc757b4be7b6a3aefeb69
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-20 16:15:47 -0800

    stuff

--- reversegam/reverse.py
+++ reversegam/reverse.py
@@ -2,3 +2,46 @@ import random
 import sys
 WIDTH = 8
 HEIGHT = 8
+
+
+def drawBoard():
+    print(' 12345678')
+    print('+--------+')
+    for y in range(HEIGHT):
+        print('%s|' % (y+1), end='')
+        for x in range(WIDTH):
+            print(board[x][y], end='')
+        print('|%s' % (y+1))
+    print(' +--------+')
+    print('  12345678')
+
+
+def getNewBoard():
+    board = []
+    for i in range(WIDTH):
+        board.append([' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])
+    return board
+
+
+def isValidMove(board, tile, xstart, ystart):
+    if board[xstart][ystart] != ' ' or not isOnBoard(xstart, ystart):
+        return False
+
+    if tile == 'X':
+        otherTile = 'O'
+    else:
+        otherTile = 'X'
+
+    tilesToFlip = []
+
+
+def isOnBoard(x, y):
+    return x >= 0 and x <= WIDTH - 1 and y >= 0 and y <= HEIGHT - 1
+
+
+def getBoardWithValidMoves(board, tile):
+    boardCopy = getBoardCopy(board)
+
+    for x, y in getValidMoves(boardCopy, tile):
+        boardCopy[x][y] = '.'
+    return boardCopy
