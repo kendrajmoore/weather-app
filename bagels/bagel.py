202e018c011d66ed2e672515dcb5586eefd686ca
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-02-23 07:47:40 -0600

    game

--- bagels/bagel.py
+++ bagels/bagel.py
@@ -3,11 +3,6 @@ import random
 NUM_DIGITS = 3
 MAX_GUESS = 10
 
-def getSecretNum():
-    
 
-'''print('I am thinking of a 3-digit number. Try to guess what it is. The clues I give are ...')
-print('When I say')
-print('Bagels means none of the digits are correct')
-print('Pico one digit is correct in the wrong position')
-print('Fermi one digit is correct and in the right position')'''
+def getSecretNum():
+    pass
