4e331d8eb185fa95a89ec249346913436ceeeb2c
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-03-01 18:35:02 -0500

    to-do

--- to-do/index.js
+++ to-do/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000,
+const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
@@ -15,6 +15,6 @@ app.get('/', function(req, res){
 
 //app.use('/api/todos', todoRoutes);
 
-app.listen(port, function() => {
-  console.log('App listening ' + process.env.PORT)
+app.listen(port, () => {
+  console.log('App listening 300')
 })
