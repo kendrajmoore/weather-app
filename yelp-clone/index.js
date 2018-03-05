54385c65c6033eab67dfb0192339d3ace0c46f5a
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-03-05 18:39:57 -0500

    yelp

--- yelp-clone/index.js
+++ yelp-clone/index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // app.js
 app.set('view engine', 'ejs')
+
+var campgrounds = [
+  {name: 'Salmon Creek', image: 'https://unsplash.com/photos/h4bBVo_CpqQ' },
+  {name:  'Goat', image: 'https://unsplash.com/photos/ZuZK8D55_cw'},
+  {name:  'Boat', image: 'https://unsplash.com/photos/aCVIH_FuyjY'}
+]
 
 app.get('/', (req, res) => {
   res.render('landing');
 });
 
 app.get('/campgrounds', (req,res) => {
-
-    var campgrounds = [
-      {name: 'Salmon Creek', image: 'https://unsplash.com/photos/h4bBVo_CpqQ' },
-      {name:  'Goat', image: 'https://unsplash.com/photos/ZuZK8D55_cw'},
-      {name:  'Boat', image: 'https://unsplash.com/photos/aCVIH_FuyjY'}
-    ]
-     res.render('campgrounds', {campgrounds: campgrounds});
+     res.render('campgrounds', { campgrounds: campgrounds});
 });
+
+app.post('/campgrounds', (req, res) => {
+  var name = req.body.name;
+  var newCampground = { name: name };
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds')
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new.ejs');
+})
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
