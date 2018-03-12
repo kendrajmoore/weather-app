d69272c1a8074b357994c02575f4f94cce1d5ff9
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-03-11 21:04:54 -0400

    add database

--- yelp-clone/index.js
+++ yelp-clone/index.js
@@ -3,9 +3,11 @@ const app         = express();
 const bodyParser  = require('body-parser');
 const mongoose    = require('mongoose');
 
-
-
+mongoose.Promise = global.Promise
 mongoose.connect('mongodb://localhost/yelp-clone');
+mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection Error:'))
+mongoose.set('debug', true)
+
 app.use(bodyParser.urlencoded({extended: true}));
 
 // app.js
@@ -16,9 +18,9 @@ const campgroundSchema = new mongoose.Schema({
   image: String
 });
 
-const Campground = mongoose.model('Campground', campgroundSchema);
+let Campground = mongoose.model('Campground', campgroundSchema);
 
-const campgrounds = [
+let campgrounds = [
   {name: 'Salmon Creek', image: 'https://res.cloudinary.com/simpleview/image/fetch/c_fill,f_auto,h_452,q_75,w_982/http://res.cloudinary.com/simpleview/image/upload/v1469218578/clients/lanecounty/constitution_grove_campground_by_natalie_inouye_417476ef-05c3-464d-99bd-032bb0ee0bd5.png' },
   {name:  'Goat', image: 'https://www.reserveamerica.com/webphotos/NH/pid270015/0/540x360.jpg'},
   {name:  'Boat', image: 'https://i.pinimg.com/originals/ab/5e/c4/ab5ec4ff722b1f65b855af71ba78c6fc.jpg'}
