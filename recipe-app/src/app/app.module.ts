9f53361db0a7738f1cf99608270c6e1d81a35640
Kendra Moore <kendra.j.moore3443@gmail.com>
2018-03-27 13:42:00 -0400

    app

--- recipe-app/src/app/app.module.ts
+++ recipe-app/src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server.component';
-import { ServersComponent } from './servers/servers.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
